7368a0fd017ddebbf56651edbaa6a60199bb379a
Austin Sharman <asharman22@gmail.com>
2023-02-03 15:06:47 -0700

    Set up elm-canvas

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import "./style.css"
 import { Elm } from "./src/Main.elm"
 
-const root = document.querySelector("#app")
-const app = Elm.Main.init({node: root});
+const root = document.querySelector("#app");
+const app = Elm.Main.init({
+    node: root,
+    flags: [window.innerWidth, window.innerHeight],
+});
--- src/Main.elm
+++ src/Main.elm
@@ -1,65 +1,68 @@
 module Main exposing (Msg(..), main)
 
 import Browser
+import Browser.Events exposing (onAnimationFrameDelta, onResize)
+import Canvas exposing (..)
+import Canvas.Settings exposing (..)
+import Color
 import Html exposing (Html)
-import Html.Attributes as Attributes
-import Html.Events as Events
 
 
 type alias Model =
-    { count : Int
+    { width : Float
+    , height : Float
     }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( { count = 0 }, Cmd.none )
+init : ( Float, Float ) -> ( Model, Cmd Msg )
+init ( width, height ) =
+    ( Model width height, Cmd.none )
 
 
-view : Model -> Html Msg
-view model =
-    Html.div [ Attributes.class "flex flex-col items-center" ]
-        [ Html.h1 [ Attributes.class "text-4xl font-bold text-blue-500" ] [ Html.text "Vite and Elm starter" ]
-        , Html.div [ Attributes.class "flex flex-col items-center" ]
-            [ Html.button
-                [ Events.onClick Increment
-                , Attributes.class "border px-8 hover:bg-slate-200"
-                ]
-                [ Html.text "+1" ]
-            , Html.div [] [ Html.text (String.fromInt model.count) ]
-            , Html.button
-                [ Events.onClick Decrement
-                , Attributes.class "border px-8 hover:bg-slate-200"
-                ]
-                [ Html.text "-1" ]
-            ]
-        ]
+view : Model -> Html msg
+view { width, height } =
+    Canvas.toHtml ( round width, round height )
+        []
+        [ shapes [ fill Color.black ] [ rect ( 0, 0 ) width height ] ]
 
 
 type Msg
-    = Increment
-    | Decrement
+    = Frame Float
+    | BrowserResized Int Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            ( { model | count = model.count + 1 }, Cmd.none )
+        BrowserResized w h ->
+            let
+                width =
+                    toFloat w
 
-        Decrement ->
-            ( { model | count = model.count - 1 }, Cmd.none )
+                height =
+                    toFloat h
+            in
+            ( { model
+                | width = width
+                , height = height
+              }
+            , Cmd.none
+            )
+
+        Frame deltaTime ->
+            ( model, Cmd.none )
 
 
-main : Program () Model Msg
+main : Program ( Float, Float ) Model Msg
 main =
-    Browser.document
-        { init = \_ -> init
+    Browser.element
+        { init = init
+        , view = view
         , update = update
-        , view =
-            \model ->
-                { title = "elm-template"
-                , body = [ view model ]
-                }
-        , subscriptions = \_ -> Sub.none
+        , subscriptions =
+            \_ ->
+                Sub.batch
+                    [ onAnimationFrameDelta Frame
+                    , onResize BrowserResized
+                    ]
         }
